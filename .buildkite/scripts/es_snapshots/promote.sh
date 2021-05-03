7852f5a490641d50feca9942b2debfbf882372c8
Brian Seeders <brian.seeders@elastic.co>
2021-05-03 16:00:09 -0400

    Add annotation

--- .buildkite/scripts/es_snapshots/promote.sh
+++ .buildkite/scripts/es_snapshots/promote.sh
@@ -4,4 +4,10 @@ set -euo pipefail
 
 export SNAPSHOT_MANIFEST="${SNAPSHOT_MANIFEST:-"$(buildkite-agent meta-data get SNAPSHOT_MANIFEST)"}"
 
+cat << EOF | buildkite-agent annotate --style "info"
+  This promotion is for the following snapshot manifest:
+
+  $SNAPSHOT_MANIFEST
+EOF
+
 node "$(dirname "${0}")/promote_manifest.js" "$SNAPSHOT_MANIFEST"
