a630a2e3e0d580820c6736b179a02cc99ac760da
Brian Seeders <brian.seeders@elastic.co>
2021-07-28 16:25:20 -0400

    Actually run the pipeline

--- .buildkite/scripts/steps/flaky_test_suite_runner.sh
+++ .buildkite/scripts/steps/flaky_test_suite_runner.sh
@@ -5,10 +5,4 @@ set -euo pipefail
 UUID="$(cat /proc/sys/kernel/random/uuid)"
 export UUID
 
-# node .buildkite/scripts/steps/flaky_test_suite_runner.js | buildkite-agent pipeline upload
-
-buildkite-agent meta-data keys
-echo '###'
-buildkite-agent meta-data get 'ftsr-override-count'
-
-node .buildkite/scripts/steps/flaky_test_suite_runner.js
+node .buildkite/scripts/steps/flaky_test_suite_runner.js | buildkite-agent pipeline upload
