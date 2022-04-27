4a3d891201bc3992515a3c01b586fd10ae6dc624
Tre' Seymour <wayne.seymour@elastic.co>
2022-04-27 16:52:11 +0100

    I just wanna see if this will work as Brian suggested, as is.

--- .buildkite/scripts/steps/code_coverage/xpack_cigroup.sh
+++ .buildkite/scripts/steps/code_coverage/xpack_cigroup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 .buildkite/scripts/bootstrap.sh
-.buildkite/scripts/build_kibana_plugins.sh
+.buildkite/scripts/download_build_artifacts.sh
 
 is_test_execution_step
 
