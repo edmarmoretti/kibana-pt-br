80916b9970b7ba3c066cf00203335daea2f20b93
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-09-07 15:24:49 +0200

    fix var name

--- .buildkite/scripts/steps/scalability/report_test_results.sh
+++ .buildkite/scripts/steps/scalability/report_test_results.sh
@@ -6,11 +6,11 @@ source .buildkite/scripts/common/util.sh
 
 echo "--- Upload Gatling reports and metadata to GCS"
 OUTPUT_REL="$(date +"%d-%m-%Y-%H:%M:%S")"
-OUTPUT_DIR="${WORKPLACE}/${OUTPUT_REL}"
+OUTPUT_DIR="${WORKSPACE}/${OUTPUT_REL}"
 GCS_BUCKET="gs://kibana-performance/scalability-test-results"
 
 download_artifact scalability_test_report.tar.gz "${OUTPUT_DIR}/"
 download_artifact meta.log "${OUTPUT_DIR}/"
 
-cd "${WORKPLACE}"
+cd "${WORKSPACE}"
 gsutil -m cp -r "${OUTPUT_REL}" "${GCS_BUCKET}"
