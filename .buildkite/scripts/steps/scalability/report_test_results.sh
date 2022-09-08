8e8b8233b6a10e5ecb01b805bc5997d006ef08d6
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-09-08 15:30:47 +0200

    create folder

--- .buildkite/scripts/steps/scalability/report_test_results.sh
+++ .buildkite/scripts/steps/scalability/report_test_results.sh
@@ -9,6 +9,8 @@ OUTPUT_REL="$(date +"%d-%m-%Y-%H:%M:%S")"
 OUTPUT_DIR="${WORKSPACE}/${OUTPUT_REL}"
 GCS_BUCKET="gs://kibana-performance/scalability-test-results"
 
+mkdir -p "${OUTPUT_DIR}"
+
 download_artifact scalability_test_report.tar.gz "${OUTPUT_DIR}/" --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
 download_artifact meta.log "${OUTPUT_DIR}/" --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
 
