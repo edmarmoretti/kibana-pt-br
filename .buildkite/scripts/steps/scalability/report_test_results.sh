a403214424b11c92c22d440a1ede7e9f5c459951
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-09-08 13:12:57 +0200

    fix artifacts download

--- .buildkite/scripts/steps/scalability/report_test_results.sh
+++ .buildkite/scripts/steps/scalability/report_test_results.sh
@@ -9,8 +9,8 @@ OUTPUT_REL="$(date +"%d-%m-%Y-%H:%M:%S")"
 OUTPUT_DIR="${WORKSPACE}/${OUTPUT_REL}"
 GCS_BUCKET="gs://kibana-performance/scalability-test-results"
 
-download_artifact scalability_test_report.tar.gz "${OUTPUT_DIR}/"
-download_artifact meta.log "${OUTPUT_DIR}/"
+download_artifact scalability_test_report.tar.gz "${OUTPUT_DIR}/" --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
+download_artifact meta.log "${OUTPUT_DIR}/" --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
 
 cd "${WORKSPACE}"
 gsutil -m cp -r "${OUTPUT_REL}" "${GCS_BUCKET}"
