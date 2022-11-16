ef07d5885b78180341c90e6d89e0cb57584639bd
lizozom <liza.katz@elastic.co>
2022-11-16 20:14:46 +0200

    Pure Elasticsearch warmup journey

--- x-pack/performance/journeys/warmup.ts
+++ x-pack/performance/journeys/warmup.ts
@@ -5,24 +5,98 @@
  * 2.0.
  */
 
+import { SearchTotalHits } from '@elastic/elasticsearch/lib/api/types';
 import { Journey } from '@kbn/journeys';
-import { subj } from '@kbn/test-subj-selector';
-import { waitForVisualizations } from '../utils';
+import expect from '@kbn/expect';
+
+const WARMUP_INDEX_NAME = 'warmup-index';
 
 export const journey = new Journey({
-  esArchives: [
-    'x-pack/performance/es_archives/sample_data_ecommerce',
-  ],
-  kbnArchives: [
-    'x-pack/performance/kbn_archives/ecommerce_no_map_dashboard',
-  ],
+  esArchives: [],
+  kbnArchives: [],
 })
-  .step('Go to Dashboards Page', async ({ page, kbnUrl }) => {
-    await page.goto(kbnUrl.get(`/app/dashboards`));
-    await page.waitForSelector('#dashboardListingHeading');
+  .step('Setup warmup index', async ({ esClient }) => {
+    await esClient.indices.create({
+      index: WARMUP_INDEX_NAME,
+      body: {
+        mappings: {
+          properties: {
+            '@timestamp': {
+              type: 'date',
+            },
+            message: {
+              type: 'keyword',
+            },
+            count: {
+              type: 'long',
+            },
+          },
+        },
+      },
+    });
+
+    await esClient.create({
+      index: WARMUP_INDEX_NAME,
+      body: {
+        '@timestamp': '2022-10-07T12:00:00.000Z',
+        message: 'Obj 1',
+        count: 1,
+      },
+      id: new Date().toISOString(),
+      refresh: true,
+    });
+
+    await esClient.create({
+      index: WARMUP_INDEX_NAME,
+      body: {
+        '@timestamp': '2022-11-07T12:00:00.000Z',
+        message: 'Obj 2',
+        count: 2,
+      },
+      id: new Date().toISOString(),
+      refresh: true,
+    });
+
+    await esClient.create({
+      index: WARMUP_INDEX_NAME,
+      body: {
+        '@timestamp': '2022-12-07T12:00:00.000Z',
+        message: 'Obj 3',
+        count: 3,
+      },
+      id: new Date().toISOString(),
+      refresh: true,
+    });
   })
 
-  .step('Go to Ecommerce Dashboard', async ({ page, log }) => {
-    await page.click(subj('dashboardListingTitleLink-[eCommerce]-Revenue-Dashboard'));
-    await waitForVisualizations(page, log, 13);
+  .step('Run search', async ({ esClient }) => {
+    const searchResponse = await esClient.search({
+      index: WARMUP_INDEX_NAME,
+      size: 3,
+    });
+
+    expect((searchResponse.hits.total as SearchTotalHits).value).to.be(3);
+
+    const asyncSearchResponse = await esClient.asyncSearch.submit({
+      wait_for_completion_timeout: '10s',
+      keep_on_completion: true,
+      index: WARMUP_INDEX_NAME,
+      aggs: {
+        the_date: {
+          date_histogram: {
+            field: '@timestamp',
+            calendar_interval: '1d',
+          },
+        },
+      },
+    });
+
+    expect((asyncSearchResponse.response.hits.total as SearchTotalHits).value).to.be(3);
+    expect(asyncSearchResponse.response.aggregations?.the_date).not.to.be(undefined);
+  })
+  .step('Cleanup', async ({ esClient }) => {
+    const resp = await esClient.indices.delete({
+      index: WARMUP_INDEX_NAME,
+    });
+    expect(resp.acknowledged).to.be(true);
   });
